6d133cea3da808787dfb46bf8e36c7ba4e25419b
sonal809 <sonalaggarwal05@gmail.com>
2014-03-14 00:28:30 -0400

    Final classification using unigram count and presence

--- Homework1/unigram.py
+++ Homework1/unigram.py
@@ -1,35 +1,33 @@
-
 from collections import Counter
 import glob
 import os
-import re
-import string
 import math
 
 class LM:
 
     def __init__(self):
-        self.badWords = ['.', ',', '(', ')']
-        """ Vocabulary of all unigram words in the training data """
+        self.badWords = ['.', ',', '(', ')', '\'']
         self.Unigramdict=[0,0]
-        """ Number of pos and neg files each """
         self.noFiles=1000
-        """ presenceMap ={unigram :[positive file count, negative file count ]  """
         self.presenceMap={}
-        """ countMap ={unigram :[total frequency in pos files, total frequency in neg files ]  """
         self.countMap={}
-        """ Total count of positive and negative unigrams , totalCount= [total pos unigrams, total neg unigrams] """
         self.totalCount=[0,0]
+        self.probability_dict_new = {}
+        self.paths = ['txt_sentoken/pos', 'txt_sentoken/neg']
+        self.probMap = {}
 
 
-    def makeUnigramMaps(self):
+    def unigrams_presence(self, start, end):
 
-        """To make the count and the presence maps from all the text files"""
+        """To count the words from all the text files in a directory"""
 
-        paths = ['txt_sentoken/pos', 'txt_sentoken/neg']
         #paths = ['test/test1', 'test/test2']
-        for i in range(len(paths)):
-            for fileobj in glob.iglob(os.path.join(paths[i], '*.txt')):
+        for i in range(len(self.paths)):
+            j = -1
+            for fileobj in glob.iglob(os.path.join(self.paths[i], '*.txt')):
+                j +=1
+                if (j>=start and j<=end):
+                    continue
                 with open (fileobj, 'r') as f:
                     txt = f.read()
                     unigrams = set(txt.split())
@@ -60,23 +58,76 @@ class LM:
                                 self.countMap[unigram] = listCount
                             self.totalCount[i] += 1
 
+
     def calculateProbs(self, mapE):
-        """ To make the probability maps of both the presence and counts of the unigrams """
-        probMap = {}
+
+        """ Computing probabilities of unigrams """
+
         if mapE == self.presenceMap:
             for word in mapE:
                 probList = mapE[word]
-                probMap[word] = [math.log(float(probList[0] + 1) / float(self.noFiles + self.Unigramdict[0])),math.log( float(probList[1] + 1)/float (self.noFiles + self.Unigramdict[1]))]
+                self.probMap[word] = [math.log(float(probList[0] + 1) / float(self.noFiles + self.Unigramdict[0])),math.log( float(probList[1] + 1)/float (self.noFiles + self.Unigramdict[1]))]
         if mapE == self.countMap:
             for word in mapE:
                 probList = mapE[word]
-                probMap[word] = [math.log(float(probList[0] + 1) / float(self.totalCount[0] + self.Unigramdict[0])),math.log( float(probList[1] + 1)/float (self.totalCount[1] + self.Unigramdict[1]))]
-        return probMap
+                self.probMap[word] = [math.log(float(probList[0] + 1) / float(self.totalCount[0] + self.Unigramdict[0])),math.log( float(probList[1] + 1)/float (self.totalCount[1] + self.Unigramdict[1]))]
 
 
-w1 = LM()
-w1.makeUnigramMaps()
-print "Count map", w1.countMap
-print   "Count values", w1.totalCount
-print "probMapPresence:", w1.calculateProbs (w1.presenceMap)
-print "probMapCount:", w1.calculateProbs(w1.countMap)
+    def test_data_categorize(self,start, end):
+
+        """ Categorizing data using reviews """
+        success_count = [0, 0]
+        for index in range(len(self.paths)):
+            j = -1
+            for fileobj in glob.iglob(os.path.join(self.paths[index], '*.txt')):
+                j +=1
+                if (j<start or j>end):
+                    continue
+                pos_prob = 0
+                neg_prob = 0
+                with open(fileobj, 'r') as f:
+                    txt = f.read()
+                    allUnigrams=txt.split()
+                    allgoodUnigrams=[item for item in allUnigrams if item not in self.badWords]
+                    for unigram in allgoodUnigrams:
+                            if unigram in self.probMap:
+                                pos_prob += self.probMap[unigram][0]
+                                neg_prob += self.probMap[unigram][1]
+                        #print pos_prob, neg_prob
+                #print pos_prob, neg_prob
+                if pos_prob > neg_prob:
+                    if index == 0:
+                        success_count[index]+=1
+                else:
+                    if index == 1:
+                        success_count[index]+=1
+
+                self.probability_dict_new[fileobj]= [pos_prob,neg_prob]
+
+
+        print "Test data:", start, "-", end
+        print "Positives:", success_count[0]," Percent Success:", success_count[0]*100/200.0
+        print "negatives:", success_count[1]," Percent Success:", success_count[1]*100/200.0
+        print "Average performance:", (success_count[0]*100/200.0 + success_count[1]*100/200.0)/2.0
+        print "\n"
+
+
+if __name__ == "__main__":
+
+    print "Text Categorization based on frequency count using unigrams"
+    for i in range(0,5):
+        start = i*200
+        end = start + 199
+        u_obj = LM()
+        u_obj.unigrams_presence(start, end)
+        u_obj.calculateProbs(u_obj.countMap)
+        u_obj.test_data_categorize(start, end)
+
+    print "Text Categorization based on presence using unigrams"
+    for i in range(0,5):
+        start = i*200
+        end = start + 199
+        u_obj = LM()
+        u_obj.unigrams_presence(start, end)
+        u_obj.calculateProbs(u_obj.presenceMap)
+        u_obj.test_data_categorize(start, end)
